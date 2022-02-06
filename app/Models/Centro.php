b14b58e8ff85113e76020a34455a07937e670aa4
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-02-06 11:24:51 +0100

    feat: :art: 11. Modelo creado
    Modelo con fillable de los campos y creando relación 1:n con User

--- app/Models/Centro.php
+++ app/Models/Centro.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Centro extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'descripcion',
+        'capacidad',
+        'entidad',
+        'extraescolar',
+        'fundado',
+        'terminos',
+        'avatar'
+    ];
+
+    public function users(){
+        return $this->belongsTo(User::class);
+    }
 }
