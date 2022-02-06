f92751b3c9203f031fa2fcc87d6a335d22d27df0
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-02-06 11:21:29 +0100

    feat: :art: 10a. Creando ruta fallback
    Ruta que manda a dashboard si algo no existe

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,9 @@ Route::get('/dashboard', function () {
 
 Route::resource('centros', CentroController::class)->middleware(['auth']);
 
+//Si la ruta no existe puedo indicar qué vista mostrar
+Route::fallback(function(){
+    return view('/dashboard');
+});
+
 require __DIR__.'/auth.php';
